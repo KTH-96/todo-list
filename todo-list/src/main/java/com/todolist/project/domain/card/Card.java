09ba50d6264973f957e2c9be783fd8d82e14e775
KTH-96 <taehyun960@gmail.com>
2022-04-14 14:27:48 +0900

    refactor: 구글 코딩 컨벤션을 사용하여 코드 포매팅 설정하기
    - 불필요한 임포트, 컨벤션 삭제

--- todo-list/src/main/java/com/todolist/project/domain/card/Card.java
+++ todo-list/src/main/java/com/todolist/project/domain/card/Card.java
@@ -1,47 +1,48 @@
 package com.todolist.project.domain.card;
 
 import com.todolist.project.domain.CardStatus;
-import lombok.Builder;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
+import lombok.Getter;
 
 @Getter
 public class Card {
-    private Long id;
-    private int cardIndex;
-    private String title;
-    private String contents;
-    private String writer;
-    private LocalDateTime createdTime;
-    private CardStatus cardStatus;
 
-    public LocalDateTime createTime() {
-        return LocalDateTime.now();
-    }
+	private Long id;
+	private int cardIndex;
+	private String title;
+	private String contents;
+	private String writer;
+	private LocalDateTime createdTime;
+	private CardStatus cardStatus;
 
-    public Card(Long id, int cardIndex, String title, String contents, String writer, LocalDateTime createdTime, CardStatus cardStatus) {
-        this.id = id;
-        this.cardIndex = cardIndex;
-        this.title = title;
-        this.contents = contents;
-        this.writer = writer;
-        this.createdTime = createdTime;
-        this.cardStatus = cardStatus;
-    }
+	public LocalDateTime createTime() {
+		return LocalDateTime.now();
+	}
 
-    public Card(int cardIndex, String title, String contents, String writer, CardStatus cardStatus) {
-        this.cardIndex = cardIndex;
-        this.title = title;
-        this.contents = contents;
-        this.writer = writer;
-        this.cardStatus = cardStatus;
-    }
+	public Card(Long id, int cardIndex, String title, String contents, String writer,
+		LocalDateTime createdTime, CardStatus cardStatus) {
+		this.id = id;
+		this.cardIndex = cardIndex;
+		this.title = title;
+		this.contents = contents;
+		this.writer = writer;
+		this.createdTime = createdTime;
+		this.cardStatus = cardStatus;
+	}
 
-    public Card(int cardIndex, String title, String contents, CardStatus cardStatus) {
-        this.cardIndex = cardIndex;
-        this.title = title;
-        this.contents = contents;
-        this.cardStatus = cardStatus;
-    }
+	public Card(int cardIndex, String title, String contents, String writer,
+		CardStatus cardStatus) {
+		this.cardIndex = cardIndex;
+		this.title = title;
+		this.contents = contents;
+		this.writer = writer;
+		this.cardStatus = cardStatus;
+	}
+
+	public Card(int cardIndex, String title, String contents, CardStatus cardStatus) {
+		this.cardIndex = cardIndex;
+		this.title = title;
+		this.contents = contents;
+		this.cardStatus = cardStatus;
+	}
 }
