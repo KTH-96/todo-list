3e2f37500644badc8b2ca2108d00cac9d15e59e4
KTH-96 <taehyun960@gmail.com>
2022-04-18 21:15:06 +0900

    feat: config클래스 만들기

--- todo-list/src/main/java/com/todolist/project/ProjectApplication.java
+++ todo-list/src/main/java/com/todolist/project/ProjectApplication.java
@@ -2,10 +2,6 @@ package com.todolist.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProjectApplication {
@@ -14,20 +10,4 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:9000")
-					.allowedMethods(
-						HttpMethod.GET.name(),
-						HttpMethod.HEAD.name(),
-						HttpMethod.POST.name(),
-						HttpMethod.PUT.name(),
-						HttpMethod.DELETE.name()
-					);
-			}
-		};
-	}
 }
---
+++ todo-list/src/main/java/com/todolist/project/config/MyConfig.java
@@ -0,0 +1,27 @@
+package com.todolist.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class MyConfig {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:9000")
+					.allowedMethods(
+						HttpMethod.GET.name(),
+						HttpMethod.HEAD.name(),
+						HttpMethod.POST.name(),
+						HttpMethod.PUT.name(),
+						HttpMethod.DELETE.name()
+					);
+			}
+		};
+	}
+}
